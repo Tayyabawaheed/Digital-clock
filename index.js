8040fc8eeb6f307a18b9fd97542078b410dcc19a
Tayyabawaheed <124914725+Tayyabawaheed@users.noreply.github.com>
2023-05-05 10:51:27 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    document.querySelector(".hours").textContent = hours;
+    document.querySelector(".minutes").textContent = minutes;
+    document.querySelector(".seconds").textContent = seconds;
+  }
+  setInterval(updateClock, 1000);
+  
+
+
+
+
